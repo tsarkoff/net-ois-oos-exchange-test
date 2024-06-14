f0aee0cbbc24f41d164b9954b29b7efb363e96d5
tsarkoff <tsarkov.vladimir@gmail.com>
2024-06-15 00:12:03 +0300

    Server: Streams / Objects initialization mover to Thread (that handles Clients). Client: Streams members declared as class-level, Console Scanner mover to separated Thread.

--- src/main/java/ru/netology/Client2.java
+++ src/main/java/ru/netology/Client2.java
@@ -5,25 +5,42 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client2 {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    private static ObjectOutputStream oos;
+    private static ObjectInputStream ois;
+    private static Scanner scanner;
+
+    public static void main(String[] args) {
         try (Socket cs = new Socket("localhost", 9999)) {
-            var out = new BufferedOutputStream(cs.getOutputStream());
-            var in = new BufferedInputStream(cs.getInputStream());
-            out.flush();
-            ObjectOutputStream oos = new ObjectOutputStream(out);
-            ObjectInputStream ois = null;
-            Scanner scanner = new Scanner(System.in);
+            oos = new ObjectOutputStream(new BufferedOutputStream(cs.getOutputStream()));
+            //oos.flush(); - вместо flush() используем отложенное создание блокирующего ObjectInputStream см. ниже if (ois == null)
+            new Thread(() -> {
+                scanner = new Scanner(System.in);
+                while (true) {
+                    System.out.print("Type name: ");
+                    String name = scanner.nextLine();
+                    Message msg = new Message(name);
+                    try {
+                        oos.writeObject(msg);
+                        oos.flush();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }).start();
+
             while (true) {
-                System.out.print("Type name: ");
-                String name = scanner.nextLine();
-                Message msg = new Message(name);
-                oos.writeObject(msg);
-                oos.flush();
-                if (ois == null)
-                    ois = new ObjectInputStream(in);
-                msg = (Message) ois.readObject();
-                System.out.println(msg);
+                try {
+                    if (ois == null)
+                        ois = new ObjectInputStream(new BufferedInputStream(cs.getInputStream()));
+                    Message msg = (Message) ois.readObject();
+                    System.out.println();
+                    System.out.println(msg);
+                } catch (IOException | ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
             }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -5,30 +5,28 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try (ServerSocket sc = new ServerSocket(9999)) {
             while (true) {
                 Socket cs = sc.accept();
-                var in = new BufferedInputStream(cs.getInputStream());
-                var out = new BufferedOutputStream(cs.getOutputStream());
-                ObjectInputStream ois = new ObjectInputStream(in);
-                ObjectOutputStream oos = new ObjectOutputStream(out);
                 new Thread(() -> {
-                    while (true) {
-                        try {
+                    try {
+                        var in = new BufferedInputStream(cs.getInputStream());
+                        ObjectInputStream ois = new ObjectInputStream(in);
+                        var out = new BufferedOutputStream(cs.getOutputStream());
+                        ObjectOutputStream oos = new ObjectOutputStream(out);
+                        //oos.flush(); - вместо flush() используем отложенное создание ObjectInputStream на Клиенте (см. Client.java)
+                        while (true) {
                             Message msg = (Message) ois.readObject();
                             System.out.println(msg);
                             oos.writeObject(msg);
                             oos.flush();
-                        } catch (IOException | ClassNotFoundException e) {
-                            throw new RuntimeException(e);
                         }
+                    } catch (IOException | ClassNotFoundException e) {
+                        throw new RuntimeException(e);
                     }
                 }).start();
             }
-        } catch (
-                IOException e) {
-            throw new RuntimeException(e);
         }
     }
 }
